dc4efea7e6b5da7205249eb5df3108174c9d72bd
Diana Buras <burasdiana@hotmail.com>
2018-06-19 14:48:21 +0200

    [add] ConsoleApp for testing Usernamepass handling

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert username:");
+            var username = Console.ReadLine();
+            while (!CheckUsername(username) )
+            {
+                Console.WriteLine("Username must be a valid email address:");
+                username = Console.ReadLine();
+            }
+            Console.WriteLine("Insert password:");
+            var password = Console.ReadLine();
+
+            var authToken = username + ":" + password;
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(authToken);
+
+            Console.WriteLine("Logging in....");
+            Console.WriteLine("Logged in as: " + ParseAuthorizationHeader(Convert.ToBase64String(plainTextBytes)).Password);
+
+            Console.ReadLine();
+        }
+
+        private static Credentials ParseAuthorizationHeader(string authHeader)
+        {
+            string[] credentials = Encoding.ASCII.GetString(Convert.FromBase64String(authHeader)).Split(new[] { ':' },2);
+
+            if (string.IsNullOrEmpty(credentials[0]) || string.IsNullOrEmpty(credentials[1]) )
+                return null;
+
+            return new Credentials() { Username = credentials[0] , Password = credentials[1]  };
+        }
+
+        private static bool CheckUsername(string username)
+        {
+            if ( username.Contains("@") && username.Contains(".com") )
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
