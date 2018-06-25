3838e4c410fcc9c2a5f9e386db17d0f057446b2b
Diana Buras <burasdiana@hotmail.com>
2018-06-25 16:25:35 +0200

    [add] code for generating an md5 hash for client login

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,23 +10,42 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert username:");
-            var username = Console.ReadLine();
-            while (!CheckUsername(username) )
-            {
-                Console.WriteLine("Username must be a valid email address:");
-                username = Console.ReadLine();
-            }
-            Console.WriteLine("Insert password:");
-            var password = Console.ReadLine();
+            //Console.WriteLine("Insert username:");
+            //var username = Console.ReadLine();
+            //while (!CheckUsername(username) )
+            //{
+            //    Console.WriteLine("Username must be a valid email address:");
+            //    username = Console.ReadLine();
+            //}
+            //Console.WriteLine("Insert password:");
+            //var password = Console.ReadLine();
 
-            var authToken = username + ":" + password;
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(authToken);
+            //var authToken = username + ":" + password;
+            //var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(authToken);
 
-            Console.WriteLine("Logging in....");
-            Console.WriteLine("Logged in as: " + ParseAuthorizationHeader(Convert.ToBase64String(plainTextBytes)).Password);
+            //Console.WriteLine("Logging in....");
+            //Console.WriteLine("Logged in as: " + ParseAuthorizationHeader(Convert.ToBase64String(plainTextBytes)).Password);
 
+            //Console.ReadLine();
+            Console.WriteLine(EncodeHashToBase64("Alex" , Generatehash("Alex" , "123" , "mdk")));
             Console.ReadLine();
+        }
+        private static string Generatehash(string username, string password, string realm)
+        {
+            var hash = String.Format(
+                    "{0}:{1}:{2}" ,
+                    username ,
+                    password ,
+                    realm).ToMd5Hash();
+            return hash;
+        }
+
+        private static string EncodeHashToBase64(string username, string hash)
+        {
+            string authstring = username + ":" +  hash;
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(authstring);
+            return System.Convert.ToBase64String(plainTextBytes);
+
         }
 
         private static Credentials ParseAuthorizationHeader(string authHeader)
