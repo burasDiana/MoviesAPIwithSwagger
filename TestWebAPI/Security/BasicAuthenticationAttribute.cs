726b3bf2696e5fa63341ff8c0ac7645a5a51f12d
Diana Buras <burasdiana@hotmail.com>
2018-09-03 12:51:32 +0200

    [add] separate authentication implementations into regions

--- TestWebAPI/Security/BasicAuthenticationAttribute.cs
+++ TestWebAPI/Security/BasicAuthenticationAttribute.cs
@@ -26,64 +26,67 @@ namespace TestWebAPI.Security
             {
                 #region basic auth implementation
                 // FORMAT=> username:password in base 64
-                //string authToken = actionContext.Request.Headers.Authorization.Parameter;
-                //string decodedAuthToken = Encoding.UTF8.GetString( Convert.FromBase64String(authToken));
-                //string[] upArray = decodedAuthToken.Split(':');
-                //string username = upArray[0];
-                //string password = upArray[1];
+                string authToken = actionContext.Request.Headers.Authorization.Parameter;
+                string decodedAuthToken = Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+                string[] upArray = decodedAuthToken.Split(':');
+                string username = upArray[0];
+                string password = upArray[1];
 
-                //if ( UserSecurity.Login(username , password) )
+                if (UserSecurity.Login(username, password))
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorized request");
+                }
+                #endregion
+
+                #region authtoken with MD5 hash implementation
+                //authtoken format => username:randomkey:hash
+                //string authToken = actionContext.Request.Headers.Authorization.Parameter;
+                //string decodedAuthToken = Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+                //string[]  upArray = decodedAuthToken.Split(new[] { ':' });
+
+                //if ( upArray.Count() != 3 )
                 //{
-                //    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                //    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized , "Authorization header format incorrect, please send in the following format  username:key:hash ");
+                //    return;
+                //}
+
+
+                //string username = upArray[0];
+                //string key = upArray[1];
+                //string password = UserSecurity.GetPasswordForUser(username);
+                //string encodedbytes = upArray[2];
+                //string domain = "mdk";
+
+                //if ( knowncodesforAlex.Contains(key) )
+                //{
+                //    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized , "Unique key already exists. Please generate a new key according to the agreed method.");
+                //    return;
                 //}
                 //else
                 //{
-                //    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorized request");
+                //    knowncodesforAlex.Add(key);
+                //}
+
+                //var hash = String.Format(
+                //    "{0}:{1}:{2}:{3}" ,
+                //    username,
+                //    password,
+                //    key,
+                //    domain).ToMd5Hash();
+
+                //if(encodedbytes.Equals(hash, StringComparison.Ordinal) )
+                //{
+                //    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username) , null);
+                //}
+                //else
+                //{
+                //    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized , "Unauthorized request, password did not match");
                 //}
                 #endregion
-                //authtoken format => username:randomkey:hash
-                string authToken = actionContext.Request.Headers.Authorization.Parameter;
-                string decodedAuthToken = Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
-                string[]  upArray = decodedAuthToken.Split(new[] { ':' });
-
-                if ( upArray.Count() != 3 )
-                {
-                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized , "Authorization header format incorrect, please send in the following format  username:key:hash ");
-                    return;
-                }
-
-               
-                string username = upArray[0];
-                string key = upArray[1];
-                string password = UserSecurity.GetPasswordForUser(username);
-                string encodedbytes = upArray[2];
-                string domain = "mdk";
-
-                if ( knowncodesforAlex.Contains(key) )
-                {
-                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized , "Unique key already exists. Please generate a new key according to the agreed method.");
-                    return;
-                }
-                else
-                {
-                    knowncodesforAlex.Add(key);
-                }
-
-                var hash = String.Format(
-                    "{0}:{1}:{2}:{3}" ,
-                    username,
-                    password,
-                    key,
-                    domain).ToMd5Hash();
-
-                if(encodedbytes.Equals(hash, StringComparison.Ordinal) )
-                {
-                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username) , null);
-                }
-                else
-                {
-                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized , "Unauthorized request, password did not match");
-                }
 
             }
         }
