195cda71aa2a2a9d73c5b6fea2fba034e4f7110b
Diana Buras <burasdiana@hotmail.com>
2018-09-18 11:09:45 +0200

    [add] return object and token object classes

---
+++ TestWebAPI/Models/AuthReturnObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebAPI.Models
+{
+    public class AuthReturnObject
+    {
+        public string TokenValue { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public string Type { get; set; }
+
+        public AuthReturnObject(string tokenValue, DateTime validFrom, DateTime validTo, string type)
+        {
+            TokenValue = tokenValue;
+            ValidFrom = validFrom;
+            ValidTo = validTo;
+            Type = type;
+        }
+    }
+}
---
+++ TestWebAPI/Security/Token.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace TestWebAPI.Security
+{
+    public class Token
+    {
+        public static string Value{ get; set; }
+        public static Dictionary<int, string> dict;
+        static string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        static Token()
+        {
+            dict = new Dictionary<int, string>();
+        }
+
+        public static string GenerateRandomNumber(int length)
+        {
+            int nrOfChars = chars.Length;
+            var stringChars = new char[length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[rnd.Next(0, chars[nrOfChars - 1])];
+            }
+            return new string(stringChars);
+        }
+
+        public static void StoreToken(int userId, string Token)
+        {
+            dict.Add(userId,Token);
+        }
+    }
+}
