f0f7ab35937d0d6a6b24904ff9ac147cecfaf251
Diana Buras <burasdiana@hotmail.com>
2018-09-19 10:18:49 +0200

    [add] ResetPasswordRequest model

---
+++ TestWebAPI/Models/ResetPasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebAPI.Models
+{
+    public class ResetPasswordRequest
+    {
+        public string UserName { get; set; }
+
+        public string OldPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
