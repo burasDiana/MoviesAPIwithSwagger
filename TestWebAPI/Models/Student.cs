c94670befe12816c08be87584187704435685ef2
Diana Buras <burasdiana@hotmail.com>
2018-10-09 23:25:00 +0200

    [add] Student class

---
+++ TestWebAPI/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebAPI.Models
+{
+    public class Student : Person
+    {
+        public string ClassId { get; set; }
+        public string ParentPhoneNr { get; set; }
+    }
+}
