cb9eaf50e886a23f8847caae074dba91baeb396b
Diana Buras <burasdiana@hotmail.com>
2018-10-22 15:44:30 +0200

    [add] query file

---
+++ SqlQueries/Add foreign key from movieId in Users table.sql
@@ -0,0 +1,10 @@
+﻿
+ALTER TABLE dbo.Users 
+ADD MovieId INT;
+
+ALTER TABLE dbo.Users 
+ADD FOREIGN KEY (MovieId) REFERENCES dbo.Movies(Id);
+
+SELECT username, email FROM dbo.Users
+JOIN Movies as m ON MovieId = m.ID
+WHERE MovieId = 5
