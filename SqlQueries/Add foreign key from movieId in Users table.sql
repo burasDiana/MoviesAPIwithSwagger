17b452c6dc4f19d732f63c1712168225f3f8bc34
Diana Buras <burasdiana@hotmail.com>
2018-10-24 08:46:25 +0200

    [add] foreign key from Movies to users
    represents relationship:  a user has many movies

--- SqlQueries/Add foreign key from movieId in Users table.sql
+++ SqlQueries/Add foreign key from movieId in Users table.sql
@@ -5,6 +5,12 @@ ADD MovieId INT;
 ALTER TABLE dbo.Users 
 ADD FOREIGN KEY (MovieId) REFERENCES dbo.Movies(Id);
 
+ALTER TABLE dbo.Users 
+ADD UserId INT;
+
+ALTER TABLE dbo.Movies 
+ADD FOREIGN KEY (UserId) REFERENCES dbo.Movies(UserId);
+
 SELECT username, email FROM dbo.Users
 JOIN Movies as m ON MovieId = m.ID
 WHERE MovieId = 5
