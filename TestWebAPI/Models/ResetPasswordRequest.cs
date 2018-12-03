53da08868d86abe137c3517228edc28592e49029
Diana Buras <burasdiana@hotmail.com>
2018-12-03 15:16:28 +0100

    [add] secret code & email address for password reset request

--- TestWebAPI/Models/ResetPasswordRequest.cs
+++ TestWebAPI/Models/ResetPasswordRequest.cs
@@ -9,8 +9,12 @@ namespace TestWebAPI.Models
     {
         public string UserName { get; set; }
 
+        public string SecretCode { get; set; }
+
         public string OldPassword { get; set; }
 
         public string NewPassword { get; set; }
+
+        public string EmailAddress { get; set; }
     }
 }
