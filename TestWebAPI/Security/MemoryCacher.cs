b73ab91a504604378e623a82c6ee80f9e04e8713
Diana Buras <burasdiana@hotmail.com>
2018-12-05 12:06:02 +0100

    [add] helper class for caching values in memory

---
+++ TestWebAPI/Security/MemoryCacher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Caching;
+
+namespace TestWebAPI.Security
+{
+    public class MemoryCacher
+    {
+        public static object GetValue(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Get(key);
+        }
+
+        public static bool Add(string key, object value, DateTimeOffset absExpiration)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Add(key, value, absExpiration);
+        }
+
+        public static void Delete(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            if (memoryCache.Contains(key))
+            {
+                memoryCache.Remove(key);
+            }
+        }
+
+        public static bool KeyExists(string key)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Contains(key) ? true : false;
+        }
+
+        public static void Set(string key, object value, DateTimeOffset absExpiration)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            memoryCache.Set(key, value, absExpiration);
+        }
+    }
+}
