266ca4067336b1b981bcf518b65c90ecda1dab9f
Diana Buras <burasdiana@hotmail.com>
2019-01-16 10:37:21 +0100

    [add] classes needed to mock database for test

---
+++ TestWebAPI.Tests/TestMovieAppContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Tests
+{
+    class TestMovieAppContext : IMovieAppContext
+    {
+        public TestMovieAppContext()
+        {
+            Movies = new TestMovieDbSet();
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+        public void MarkAsModified(Movie item)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
---
+++ TestWebAPI.Tests/TestMovieDbSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Tests
+{
+    class TestMovieDbSet : TestDBSet<Movie>
+    {
+        public override Movie Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(movie => movie.ID == (int) keyValues.Single());
+        }
+    }
+}
