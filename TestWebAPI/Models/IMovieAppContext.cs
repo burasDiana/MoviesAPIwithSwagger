a80d779b7e5df62d8fddb6d49aedc49c3530ac4e
Diana Buras <burasdiana@hotmail.com>
2019-01-16 10:36:43 +0100

    [add] classes to establish db context

---
+++ TestWebAPI/Models/IMovieAppContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace TestWebAPI.Models
+{
+    public interface IMovieAppContext : IDisposable
+    {
+        DbSet<Movie> Movies { get; }
+        int SaveChanges();
+        void MarkAsModified(Movie item);
+    }
+}
---
+++ TestWebAPI/Models/TestWebAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+
+namespace TestWebAPI.Models
+{
+    public class TestWebAPIContext : DbContext , IMovieAppContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TestWebAPIContext() : base("name=TestWebAPIContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public void MarkAsModified(Movie item)
+        {
+            Entry(item).State = EntityState.Modified;
+        }
+    }
+}
