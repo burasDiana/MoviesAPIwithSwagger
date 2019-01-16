9f8a0e42f213ffb7b106f736a55d21f610553310
Diana Buras <burasdiana@hotmail.com>
2019-01-16 10:37:01 +0100

    [add] movie controller class for tests

---
+++ TestWebAPI/Controllers/MoviesV2Controller.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Controllers
+{
+    public class MoviesV2Controller : ApiController
+    {
+        //private TestWebAPIContext db = new TestWebAPIContext();
+
+        private IMovieAppContext db = new TestWebAPIContext();
+
+        public MoviesV2Controller()
+        {
+
+        }
+
+        public MoviesV2Controller(IMovieAppContext context)
+        {
+            db = context;
+        }
+
+
+        // GET: api/MoviesV2
+        public IQueryable<Movie> GetMovies()
+        {
+            return db.Movies;
+        }
+
+        // GET: api/MoviesV2/5
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult GetMovie(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        // PUT: api/MoviesV2/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != movie.ID)
+            {
+                return BadRequest();
+            }
+
+           // db.Entry(movie).State = EntityState.Modified;
+            db.MarkAsModified(movie);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/MoviesV2
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult PostMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Movies.Add(movie);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = movie.ID }, movie);
+        }
+
+        // DELETE: api/MoviesV2/5
+        [ResponseType(typeof(Movie))]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+
+            return Ok(movie);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MovieExists(int id)
+        {
+            return db.Movies.Count(e => e.ID == id) > 0;
+        }
+    }
+}
