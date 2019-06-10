93f5dbf4a7bf18311eda996ab10057cda60e6bbb
Diana Buras <burasdiana@hotmail.com>
2019-06-10 15:01:01 +0200

    [add] ChatMessageCreateExample

---
+++ TestWebAPI/Models/ExampleModels/ChatMessageModelExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Swashbuckle.Examples;
+using TestWebAPI.Models.ResponseModels;
+
+namespace TestWebAPI.Models.ExampleModels
+{
+
+    public class ChatMessageCreateExample : ChatMessageCreate, IExamplesProvider
+    {
+        public object GetExamples()
+        {
+            return new ChatMessageCreateExample()
+            {
+                Message = "This is a great message to someone.",
+                ReceiverId = 12346,
+            };
+        }
+    }
+}
