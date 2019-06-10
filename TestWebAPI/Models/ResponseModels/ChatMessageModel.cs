cb3af2321bca7021a5b25ff8476a9d5634341c6c
Diana Buras <burasdiana@hotmail.com>
2019-06-10 14:59:53 +0200

    [add] chatmessage model

---
+++ TestWebAPI/Models/ResponseModels/ChatMessageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestWebAPI.Models.ResponseModels
+{
+    public class ChatMessageModel : ChatMessageCreate
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateRead { get; set; }
+    }
+
+  
+}
