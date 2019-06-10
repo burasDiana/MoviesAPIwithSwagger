bd7825037ddb91e50403840ab15f6cce0ffff043
Diana Buras <burasdiana@hotmail.com>
2019-06-10 14:58:58 +0200

    [add] validation filter

---
+++ TestWebAPI/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace TestWebAPI.Filters
+{
+    /// <summary>
+    /// Validates the model properties according to the rules set in the models
+    /// </summary>
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var nullObj = actionContext.ActionArguments.Where(kv => kv.Value == null)
+                .Select(kv => $"The argument '{kv.Key}' cannot be null");
+
+            if (nullObj.Any())
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+                    (HttpStatusCode) 422, string.Join("\n", nullObj));
+            }
+
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+                    (HttpStatusCode) 422, actionContext.ModelState);
+            }
+
+        }
+    }
+}
