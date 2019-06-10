a537015d8d5acd65d115a94489367061a7b107f8
Diana Buras <burasdiana@hotmail.com>
2019-06-10 15:01:16 +0200

    [add] ChatMessageModelExample

--- TestWebAPI/Models/ExampleModels/ChatMessageModelExample.cs
+++ TestWebAPI/Models/ExampleModels/ChatMessageModelExample.cs
@@ -7,6 +7,30 @@ using TestWebAPI.Models.ResponseModels;
 
 namespace TestWebAPI.Models.ExampleModels
 {
+    public class ChatMessageModelExample : ChatMessageCreate, IExamplesProvider
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateRead { get; set; }
+
+        public object GetExamples()
+        {
+            var current_year = DateTime.Now.ToString("yyyy");
+            var current_month = DateTime.Now.AddMonths(1).ToString("MM");
+            DateTime dateCreated = Convert.ToDateTime($"{current_year}-{current_month}-01T09:00:00");
+
+            return new ChatMessageModelExample()
+            {
+                Id = 12456,
+                SenderId = 87905,
+                ReceiverId = 42587,
+                DateCreated = dateCreated,
+                DateRead = null,
+                Message = "Have a nice day!"
+            };
+        }
+    }
 
     public class ChatMessageCreateExample : ChatMessageCreate, IExamplesProvider
     {
